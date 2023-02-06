48743db13359d3fbb8ed6253b6cbf37621bd6f46
SohanaN <sohananaz01@gmail.com>
2023-02-07 01:02:39 +0600

    separate main content from home page to new folder & file

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,7 +5,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
 
     public function index() {
-        $this->load->view('master');
+        $data = array();
+        // first parameter page er location
+        // second parameter array hoy jodi kono data pass korte hoy noito '' blank rakte hoy
+        // third parameter true dile page er code return hoy
+        $data['main_content'] = $this->load->view('pages/home_content', '', true);
+        $this->load->view('master', $data);
     }
 
 }
